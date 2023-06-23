8088dd3e02ef1a953e4c4ab5160c3e43e7106584
BogdanPhoenix <61631220+BogdanPhoenix@users.noreply.github.com>
2023-06-23 23:01:20 +0300

    Delete Acer Aspire 5 without Nvidia/archiso directory

--- Acer Aspire 5 without Nvidia/archiso/airootfs/etc/skel/Script_D_pacman.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pacman -Syu vim make cmake htop java-commons-daemon java-environment-common java-jsvc java-runtime-common python python-pip gcc
--- Acer Aspire 5 without Nvidia/archiso/airootfs/root/Script_All.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./Script_A_createSections.sh
-./Script_B_download_pacstrap.sh
-./Script_C_installArch.sh
